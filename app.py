8d9d81e0275ca69633eeae6a5c72cefa335dffc3
josephlarrivy <joseph.larrivy@gmail.com>
2022-10-25 06:45:35 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Tweet
+from forms import UserForm, TweetForm
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///29.5-hashing-and-log-in"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
